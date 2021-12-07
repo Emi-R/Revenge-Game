e2327689b52ca5ac7213188a7d329de27d817339
Myxolidian <88684755+Myxolidian@users.noreply.github.com>
2021-12-06 23:20:48 -0300

    vers 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*****************************************************
- 	Proyecto Final Laboratorio de Computacion I UTN (FRGP)    
+Proyecto Final Laboratorio de Computacion I UTN (FRGP)    
  													 
 		Juego: "Revenge"					 
 													 
